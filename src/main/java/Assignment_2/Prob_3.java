43e8d6e5dd760d3c2eba040e420083cf1ffa0fd6
oindrila7 <43373988+oindrila7@users.noreply.github.com>
2021-08-15 21:52:28 +0600

    update

--- src/main/java/Assignment_2/Prob_3.java
+++ src/main/java/Assignment_2/Prob_3.java
@@ -6,5 +6,33 @@
 
 package Assignment_2;
 
+import java.util.Scanner;
 public class Prob_3 {
+    public static void main(String[] args) {
+        int iteration=5;
+        int min = 10;
+        int max = 99;
+
+        int count = 1, score = 0;
+        while (count <= 5)
+        {
+            double rand1 = Math.random() * (max - min) + min;
+            double rand2 = Math.random() * (max - min) + min;
+            int num1 = (int) rand1;
+            int num2 = (int) rand2;
+            System.out.println("Question "+count+":\nWhat is the sum of " + num1 + " and " + num2 + "?");
+
+            Scanner scanner = new Scanner(System.in);
+            int sum = scanner.nextInt();
+
+            if (num1 + num2 == sum) {
+                System.out.printf("Correct!\n");
+                score++;
+            } else {
+                System.out.println("Wrong");
+            }
+            count++;
+        }
+        System.out.println("Quiz end! your total score is "+score+" out of "+iteration);
+    }
 }
--- src/main/java/Assignment_2/prob_4.java
+++ src/main/java/Assignment_2/prob_4.java
@@ -6,5 +6,66 @@
 
 
 package Assignment_2;
-public class prob_4 {
+import java.util.Scanner;
+public class prob_4{
+    public static void main(String[] args) {
+        int PIN = 1234;
+        double balance = 5000;
+        System.out.println("1. Check balance");
+        System.out.println("2. Withdraw balance");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                int count = 0;
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("Enter your PIN");
+                    int pin = scanner.nextInt();
+                    if (PIN == pin) {
+                        System.out.println("Balance is " + balance + " Tk");
+                        break;
+                    } else {
+                        System.out.println("Wrong PIN inserted");
+                    }
+                    count++;
+
+                }
+                if (count >= 3) {
+                    System.out.println("You have entered wrong PIN more than 3 times. Card has blocked.");
+                }
+                break;
+            case 2:
+                count = 0;
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("Enter your PIN");
+                    int pin = scanner.nextInt();
+                    if (PIN == pin) {
+                        System.out.println("Input amount that multiply by 500");
+                        int amount = scanner.nextInt();
+                        if (amount <= balance && amount % 500 == 0) {
+                            balance -= amount;
+                            System.out.println("Withdraw successful. Your current balance is " + balance + " Tk");
+                            break;
+                        } else if (amount > balance) {
+                            System.out.println("Not enough balance. Please input valid amount");
+                        } else {
+                            System.out.println("Balance is not multiply by 500. Please try with another amount");
+                        }
+                    } else if (PIN != pin) {
+                        System.out.println("Wrong PIN inserted");
+                    } else if (count >= 3) {
+                        System.out.println("You have entered wrong PIN more than 3 times. Card has blocked.");
+                    }
+                    count++;
+
+                }
+                if (count >= 3) {
+                    System.out.println("You have entered wrong PIN more than 3 times. Card has blocked.");
+                }
+                break;
+            default:
+                System.out.println("Please select option either 1 or 2");
+
+        }
+    }
 }
